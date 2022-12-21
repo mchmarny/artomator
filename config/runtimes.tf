607da99ac87eeab783df3a5b4c3fc39d06384a60
Mark Chmarny <mark@chmarny.com>
2022-12-21 10:51:38 -0800

    tf cleanup

--- config/runtimes.tf
+++ config/runtimes.tf
@@ -28,155 +28,80 @@ resource "google_project_iam_member" "runner_role_bindings" {
   member   = "serviceAccount:${google_service_account.runner_service_account.email}"
 }
 
-# Worker Cloud Run service 
-resource "google_cloud_run_service" "worker" {
-  name                       = "${var.name}-worker"
-  location                   = var.region
-  project                    = var.project_id
-  autogenerate_revision_name = true
+# # Worker Cloud Run service 
+# resource "google_cloud_run_service" "worker" {
+#   name                       = var.name
+#   location                   = var.region
+#   project                    = var.project_id
+#   autogenerate_revision_name = true
 
-  template {
-    spec {
-      containers {
-        image = "${var.worker_image}:${data.template_file.version.rendered}"
-        volume_mounts {
-          name       = "config-secret"
-          mount_path = "/secrets"
-        }
-        ports {
-          name           = "http1"
-          container_port = 8080
-        }
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
-        env {
-          name  = "ADDRESS"
-          value = ":8080"
-        }
-        env {
-          name  = "CONFIG"
-          value = "/secrets/${var.name}"
-        }
-      }
-      volumes {
-        name = "config-secret"
-        secret {
-          secret_name = google_secret_manager_secret.config_secret.secret_id
-          items {
-            key  = var.config_secret_version
-            path = var.name
-          }
-        }
-      }
+#   template {
+#     spec {
+#       containers {
+#         image = "${var.location}-docker.pkg.dev/${var.project_id}/${var.name}/${var.name}:${data.template_file.version.rendered}"
 
-      container_concurrency = 80
-      timeout_seconds       = 900
-      service_account_name  = google_service_account.runner_service_account.email
-    }
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale" = "3"
-      }
-    }
-  }
+#         ports {
+#           name           = "http1"
+#           container_port = 8080
+#         }
+#         resources {
+#           limits = {
+#             cpu    = "1000m"
+#             memory = "2048Mi"
+#           }
+#         }
+#         env {
+#           name  = "ADDRESS"
+#           value = ":8080"
+#         }
+#         env {
+#           name  = "CONFIG"
+#           value = "/secrets/${var.name}"
+#         }
+#       }
+#       volumes {
+#         name = "config-secret"
+#         secret {
+#           secret_name = google_secret_manager_secret.config_secret.secret_id
+#           items {
+#             key  = var.config_secret_version
+#             path = var.name
+#           }
+#         }
+#       }
 
-  metadata {
-    annotations = {
-      "run.googleapis.com/client-name" = "terraform"
-      "run.googleapis.com/ingress"     = "all"
-      # all, internal, internal-and-cloud-load-balancing
-    }
-  }
+#       container_concurrency = 80
+#       timeout_seconds       = 900
+#       service_account_name  = google_service_account.runner_service_account.email
+#     }
+#     metadata {
+#       annotations = {
+#         "autoscaling.knative.dev/maxScale" = "3"
+#       }
+#     }
+#   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+#   metadata {
+#     annotations = {
+#       "run.googleapis.com/client-name" = "terraform"
+#       "run.googleapis.com/ingress"     = "all"
+#       # all, internal, internal-and-cloud-load-balancing
+#     }
+#   }
 
-  depends_on = [google_secret_manager_secret_version.config_secret_version]
-}
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+
+#   depends_on = [google_secret_manager_secret_version.config_secret_version]
+# }
 
 
-# App Cloud Run service 
-resource "google_cloud_run_service" "app" {
-  name                       = "${var.name}-app"
-  location                   = var.region
-  project                    = var.project_id
-  autogenerate_revision_name = true
-
-  template {
-    spec {
-      containers {
-        image = "${var.app_image}:${data.template_file.version.rendered}"
-        volume_mounts {
-          name       = "config-secret"
-          mount_path = "/secrets"
-        }
-        ports {
-          name           = "http1"
-          container_port = 8080
-        }
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
-        env {
-          name  = "ADDRESS"
-          value = ":8080"
-        }
-        env {
-          name  = "CONFIG"
-          value = "/secrets/${var.name}"
-        }
-      }
-      volumes {
-        name = "config-secret"
-        secret {
-          secret_name = google_secret_manager_secret.config_secret.secret_id
-          items {
-            key  = var.config_secret_version
-            path = var.name
-          }
-        }
-      }
-
-      container_concurrency = 80
-      timeout_seconds       = 120
-      service_account_name  = google_service_account.runner_service_account.email
-    }
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale" = "10"
-      }
-    }
-  }
-
-  metadata {
-    annotations = {
-      "run.googleapis.com/client-name" = "terraform"
-      "run.googleapis.com/ingress"     = "internal-and-cloud-load-balancing"
-      # all, internal, internal-and-cloud-load-balancing
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-
-  depends_on = [google_secret_manager_secret_version.config_secret_version]
-}
-
-resource "google_cloud_run_service_iam_member" "app-public-access" {
-  location = google_cloud_run_service.app.location
-  project  = google_cloud_run_service.app.project
-  service  = google_cloud_run_service.app.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
+# resource "google_cloud_run_service_iam_member" "app-public-access" {
+#   location = google_cloud_run_service.app.location
+#   project  = google_cloud_run_service.app.project
+#   service  = google_cloud_run_service.app.name
+#   role     = "roles/run.invoker"
+#   member   = "serviceAccount:${google_service_account.runner_service_account.email}"
+# }
